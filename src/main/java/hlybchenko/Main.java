6c4a4d0d349a8af538c18fb16062cdadeedf34b7
Andrii <hlybchenko.andrii@gmail.com>
2022-07-31 21:23:54 +0300

    Removed extra code in pom.xml. Added try -catch block when working with XPath. Fixed for loop when working with collections. Other minor change.

--- src/main/java/hlybchenko/Main.java
+++ src/main/java/hlybchenko/Main.java
@@ -10,10 +10,7 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-
-    static Duration duration = Duration.ofSeconds(5);
-
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         System.out.println("What video card do you want to find?");
         String searchInputValue = new Scanner(System.in).nextLine();
@@ -26,24 +23,34 @@ public class Main {
         element.sendKeys(searchInputValue);
 
         // Click to search button
-        elementClicker(driver, "//header/div/div/div/form/button", 2000);
+        try {
+            elementClicker(driver, "//header/div/div/div/form/button", 2000);
+        } catch (Exception e) {
+            System.out.println("Incorrect enter value.");
+        }
 
         // Click to "only video card"
-        elementClicker(driver, "//*[text()[contains(.,'Відеокарти')]]", 2000);
+        try {
+            elementClicker(driver, "//*[text()[contains(.,'Відеокарти')]]", 1000);
+        } catch (Exception e) {
+            System.out.println("Attention: *Video card* category not available for selection.");
+        }
 
         // Click to "seller only Rozetka"
         try {
             elementClicker(driver, "//*[contains(@class, 'checkbox-filter__link') and contains(., 'Rozetka')]",
-                    1000);
-        }catch (Exception e) {
-            System.out.println("Vendor selection not available.");
+                    3000);
+        } catch (Exception e) {
+            System.out.println("Attention: Vendor selection not available.");
         }
 
+        // Create several collections of the Leaf type and save the result
         List<WebElement> description = driver.findElements(By.className("goods-tile__title"));
         List<WebElement> links = driver.findElements(By.className("goods-tile__heading"));
         List<WebElement> price  = driver.findElements(By.className("goods-tile__price-value"));
 
-        for (int i = 1; i <= description.size()-20; i++) {
+        // Outputting collected information to the console
+        for (int i = 0; i < description.size(); i++) {
             System.out.println("------\nDescription: " + description.get(i).getText() +
                     "\nPrice: " +  price.get(i).getText() + " UAH.\n" + links.get(i).getAttribute("href"));
         }
@@ -58,10 +65,11 @@ public class Main {
      * Here we configure the driver and declare the parameters of the program.
      */
     private static WebDriver driverConfig() {
+        var driverDuration = Duration.ofSeconds(5);
         System.setProperty("webdriver.chrome.driver", "drivers\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().pageLoadTimeout(duration);
-        driver.manage().timeouts().scriptTimeout(duration);
+        driver.manage().timeouts().pageLoadTimeout(driverDuration);
+        driver.manage().timeouts().scriptTimeout(driverDuration);
         driver.manage().window().maximize();
         return driver;
     }
